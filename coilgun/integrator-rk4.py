57b8505e4513cba4e76c45fb5d112b966893fd2c
Calvin Yang <calvin.yang.jz@gmail.com>
2026-09-02 13:04:52 -0700

    Refactored the RK4 integrator to accept the derivative function as an argument

--- coilgun/integrator-rk4.py
+++ coilgun/integrator-rk4.py
@@ -4,14 +4,18 @@ V0, R, C, dt, steps = 5.0, 100, 1e-4, 1e-6, 20
 t, V = [0], [V0]
 
 
-def rk4_step(v_current, dt, R, C):
-    k1_dvdt = -v_current / (R * C)
+def derivative_func(v, R, C):
+    return -v / (R * C)
+
+
+def rk4_step(v_current, dt, derivative_func):
+    k1_dvdt = derivative_func(v_current)
     vmid_a = v_current + k1_dvdt * (dt / 2)
-    k2_dvdt = -vmid_a / (R * C)
+    k2_dvdt = derivative_func(vmid_a)
     vmid_b = v_current + k2_dvdt * (dt / 2)
-    k3_dvdt = -vmid_b / (R * C)
+    k3_dvdt = derivative_func(vmid_b)
     vend = v_current + k3_dvdt * dt
-    k4_dvdt = -vend / (R * C)
+    k4_dvdt = derivative_func(vend)
     k_avg_dvdt = (k1_dvdt + 2 * k2_dvdt + 2 * k3_dvdt + k4_dvdt) / 6
     final_voltage = v_current + k_avg_dvdt * dt
     return final_voltage
@@ -19,7 +23,7 @@ def rk4_step(v_current, dt, R, C):
 
 for i in range(steps):
     v_current = V[-1]
-    V.append(rk4_step(v_current, dt, R, C))
+    V.append(rk4_step(v_current, dt, lambda v: derivative_func(v, R, C)))
     t.append(t[-1] + dt)
 
 print(f"Final voltage: {V[-1]:.6f} V")
